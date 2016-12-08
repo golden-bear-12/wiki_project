e37ccb9301b601a95ed0161bfaf820001bb81383
golden-bear-12 <m.honrychs@gmail.com>
2016-12-07 22:54:01 -0500

    faker seed database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
-private
+  private
 
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'faker'
+
+5.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  )
+end
+users = User.all
+
+15.times do
+  Wiki.create!(
+  user: users.sample,
+  title: Faker::Lorem.words(1),
+  body: Faker::Lorem.paragraph
+  #private: Faker::Boolean.boolean(0.1)
+  )
+end
+wikis = Wiki.all
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Wiki.count} wikis created"
